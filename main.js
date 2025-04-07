c84dfbf1ccd7a6e6f70e2b108ccc438fc1801b11
Brian Calma <briancalmadevacc@gmail.com>
2025-04-07 21:47:22 +0800

    feat: added update and edit todo item

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
 var todos = [
     {
         id: 1,
-        title: "Buy groceries",
+        title: "Eat",
         isCompleted: false,
     }, 
     {
         id: 2,
-        title: "Buy groceries",
+        title: "Code",
         isCompleted: false,
     },
     {
         id: 3,
-        title: "Buy groceries",
+        title: "Sleep",
+        isCompleted: false,
+    },
+    {
+        id: 4,
+        title: "Repeat",
         isCompleted: false,
     },
 ];
 
-// Todo: Please render the default todos here :) 
+var selectedTodo = null;
 
 function renderTodos() {
     var todosContainer = document.getElementById("todos");
@@ -28,8 +33,8 @@ function renderTodos() {
             <td>${ todo.id }</td>
             <td>${ todo.title }</td>
             <td>
-                <button class="btn btn-primary">Edit</button>
-                <button class="btn btn-danger">Delete</button>
+                <button class="btn btn-primary" onClick="editTodo(${todo.id})">Edit</button>
+                <button class="btn btn-danger" onClick="deleteTodo(${todo.id})">Delete</button>
             </td>
         </tr>`;
 
@@ -37,25 +42,83 @@ function renderTodos() {
     });
 }
 
-document.addEventListener('DOMContentLoaded', function(event) {
+function deleteTodo(id) {
+    const index = todos.findIndex(function(todo) {
+        return todo.id === id;
+    });
+
+    todos.splice(index, 1);
+    renderTodos();
+}
+
+function editTodo(id) {
+    const todo = todos.find(function(todo) {
+        return todo.id === id;
+    });
+
+    selectedTodo = todo;
+
+    var title = document.getElementById("title");
     var todoForm = document.getElementById("todoForm");
+    var submitButton = document.getElementById("submitButton");
+    
+    title.value = selectedTodo.title;
+    submitButton.innerText = 'Update';
+}
+
+function updateTodo() {
+    var todoForm = document.getElementById("todoForm");
+
+    var title = document.getElementById("title");
+    var newTitle = title.value;
+
+    // Update the todo title
+    selectedTodo.title = newTitle;
+
+    const index = todos.findIndex(function(todo) {
+        return todo.id === selectedTodo.id;
+    });
+    
+    todos[index] = selectedTodo;
+
+    renderTodos();
+
+    // Reset the form
+    selectedTodo = null;
+    todoForm.reset();
+    submitButton.innerText = 'Submit';
+}
+
+function addTodo() {
+    var todoForm = document.getElementById("todoForm");
+
+    var title = document.getElementById("title");
+    // generate random id
+    var date = new Date();
+    var id = date.getTime();
+
+    todos.push({
+        id: id,
+        title: title.value,
+        isCompleted: false,
+    });
+    
+    todoForm.reset();
+
+    renderTodos();
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    renderTodos();
+
 
     todoForm.addEventListener('submit', function(e) {
         e.preventDefault();
 
-        var title = document.getElementById("title");
-        // generate random id
-        var date = new Date();
-        var id = date.getTime();
-
-        todos.push({
-            id: id,
-            title: title.value,
-            isCompleted: false,
-        });
-        
-        todoForm.reset();
-
-        renderTodos();
+        if(selectedTodo !== null) {
+            updateTodo();
+        } else {
+           addTodo();
+        }
     });
 });
